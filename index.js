a00076dfc2ffb9238a4917521fd494b099139e67
April Darger <april.m.darger@gmail.com>
2020-09-17 13:43:44 -0600

    Finished task one. Working on adding distance to task 2.

--- index.js
+++ index.js
@@ -42,7 +42,22 @@ class Airplane {
 */
 
 class Person {
-
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  eat(food) {
+    if (this.stomach.length < 10) {
+      this.stomach.push(food);
+    }
+  }
+  poop() {
+    this.stomach = [];
+  }
+  toString() {
+    return `${this.name}, ${this.age}`;
+  }
 }
 
 /*
@@ -60,7 +75,15 @@ class Person {
 */
 
 class Car {
-
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons) {
+    this.gallons = this.tank + gallons;
+  }
 }
 
 /*
